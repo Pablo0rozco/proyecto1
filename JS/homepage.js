db0d7f0bd76fa6df16d2e0fe5387b5f573719c1c
pol1987 <pabloorozcolopez@gmail.com>
2022-11-22 18:03:55 +0100

    render detailled info in projects page

--- JS/homepage.js
+++ JS/homepage.js
@@ -1,3 +1,5 @@
+let globalData;
+
 function fetchData() {
   fetch(
     "https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects"
@@ -7,21 +9,44 @@ function fetchData() {
     })
     .then((data) => {
       modifyData(data);
+      globalData = data;
       console.log("Parsed response: ", data);
     })
     .catch((err) => console.log(err));
 }
 
 function modifyData(data) {
-  let title = document.querySelectorAll(".project-name");
+  let title = document.querySelectorAll(".fetch-project h3");
   let description = document.querySelectorAll(".fetch-project p");
   let image = document.querySelectorAll(".fetch-project img");
 
-  for (let i = 0; i < title.length; i++) {
-    title[i].innerText = data[i].name;
-    description[i].innerText = data[i].description;
-    image[i].src = data[i].image;
+  let counter = 1;
+  for (let i = title.length - 1; i >= 0; i--) {
+    title[i].innerText = data[counter].name;
+    description[i].innerText = data[counter].description;
+    image[i].src = data[counter].image;
+    counter++;
   }
 }
 
 fetchData();
+
+let title = document.querySelector("#section-simplify h1");
+let description = document.querySelector("#section-simplify h2");
+let image = document.querySelector(".section-simplify-img");
+let content = document.querySelector("#section-simplify p");
+let date = document.querySelector(".data-holder");
+
+console.log(title.innerText);
+console.log(description.innerText);
+console.log(image.src);
+console.log(content.innerText);
+console.log(date.innerText);
+
+function setDataProject(pos) {
+  date.innerText = globalData[pos].completed_on;
+  title.innerText = globalData[pos].name;
+  description.innerText = globalData[pos].description;
+  image.src = globalData[pos].image;
+  content.innerText = globalData[pos].content;
+}
