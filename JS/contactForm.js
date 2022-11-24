74a31a2cab7b4ba5dc6945d1348d07ceff1c9b5d
pol1987 <78923863+pol1987@users.noreply.github.com>
2022-11-24 11:53:17 +0100

    Contact page css fixing (#4)
    * Homepage CSS Fix Style & Responsive
    
    * Project page CSS Fix Style & Responsive
    
    * Contact page Css Fix Style & Responsive
    
    * project page css fixing & checking input box

---
+++ JS/contactForm.js
@@ -0,0 +1,16 @@
+const form = document.getElementById("form");
+const fullname = document.getElementById("fullname");
+
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();  
+
+  if (fullname.value === "ironhack") {
+    alert("You cannot be Ironhack, because I am Ironhack.");
+  } else {
+    fullname.value = "";
+  }
+
+
+});
+
